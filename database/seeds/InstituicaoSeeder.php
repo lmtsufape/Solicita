8a52a97185bd63e787061f64cd5248efa175ba98
root <ihagog12@gmail.com>
2019-09-12 18:06:59 -0300

    Criação de Seeds(Administrador, Curso, Documentos, Instituicao, Unidade e Usuário)

---
+++ database/seeds/InstituicaoSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class InstituicaoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+      $administrador_id = DB::table('administradors')->where('matricula','123456789')->pluck('id');
+        DB::table('instituicaos')->insert([
+          'nome'=>'Universidade Federal Rural de Pernambuco',
+          'administrador_id'=>$administrador_id[0],
+        ]);
+    }
+}
