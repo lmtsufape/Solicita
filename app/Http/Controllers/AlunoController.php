3873cc7b4da072f0696bf8b99cdbfcd34dcd5aa8
lmts <jvsveloso@gmail.com>
2019-09-25 14:22:33 -0300

    Atualização

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -1,16 +1,12 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-
 use App\Curso;
 use App\Aluno;
 use App\User;
 use App\Perfil;
 use App\Unidade;
-
 class AlunoController extends Controller
 {
   // Redireciona para tela de login ao entrar no sistema
@@ -18,52 +14,6 @@ class AlunoController extends Controller
   {
     return view('autenticacao.login');
   }
-
-<<<<<<< HEAD
-  //cadastro de aluno
-  public function createAluno(){
-
-    $cursos = Curso::all();
-    $unidades = Unidade::all();
-
-    return view('autenticacao.cadastro',compact('cursos','unidades')); //redireciona para view de cadastro do aluno
-  }
-
-  public function storeAluno(Request $request){
-
-    $regras = [
-      'name' => 'required|string|max:255',
-      //'cpf' => ['required','integer','size:11','unique:alunos'],
-      'email' => 'required|string|email|max:255|unique:users',
-      'password' => 'required|string|min:8|confirmed',
-      'vinculo' => ['required'],
-      'unidade' => ['required'],
-      'cursos' => ['required'],
-    ];
-    $mensagens = [
-      'name.required' => 'Por favor, preencha este campo',
-      'email.required' => 'Por favor, preencha este campo',
-      'email.email' => 'Por favor, preencha um email válido',
-      'vinculo.required' => 'Por favor, selecione o tipo de vínculo',
-      'unidade.required' => 'Por favor, selecione a unidade acadêmica',
-      'cursos.required' => 'Por favor, selecione o seu curso',
-      'password.required' => 'Por favor, digite uma senha',
-      'passowd.min' => 'Por favor, digite uma senha com, no mínimo, 8 dígitos',
-
-    ];
-
-    //$request->validate([$regras,$mensagens]);
-    $request->validate([
-      'name' => 'required|string|max:255',
-      'cpf' => 'required|cpf|unique:alunos',
-      'email' => 'required|string|email|max:255|unique:users',
-      'password' => 'required|string|min:8|confirmed',
-      'vinculo' => ['required'],
-      'unidade' => ['required'],
-      'cursos' => ['required'],
-    ]);
-
-=======
   public function preparaNovaRequisicao(Request $request){
     // $unidades = Unidade::where('nome', $request->$nome)->first();
     $unidades = Unidade::All();
@@ -82,17 +32,14 @@ class AlunoController extends Controller
       $documentos = new Documento();
       // $nome_aluno = $request->input('nomeAluno');
       //dd($nome_aluno);
-
       $documentos->tipo = 'Declaracao de Vínculo';
       $documentos->save();
-
       $requisicao->data_pedido = '20/09/2019';
       $requisicao->hora_pedido = '00:00:00';
       $requisicao->aluno_id = 1;
       $requisicao->perfil_id = 1;
       $requisicao->servidor_id = 1;
       $requisicao->save();
-
       $documento_req->documento_id = $documentos->id;
       $documento_req->requisicao_id = $requisicao->id;
       $documento_req->aluno_id = 1;
@@ -102,7 +49,6 @@ class AlunoController extends Controller
       $documento_req->status_data = '20/09/2019';
       $documento_req->detalhes = 'Anotacoes para o servidor';
       $documento_req->save();
-
       return view('autenticacao.confirmacao-requisicao', compact('documentos', 'requisicao', 'documento_req'));
     }
     public function confirmacaoRequisicao(Request $request){
@@ -118,22 +64,13 @@ class AlunoController extends Controller
     public function home(){
       return redirect('home-aluno');
     }
-
-
-
-
-
   //cadastro de aluno
   public function createAluno(){
-
     $cursos = Curso::all();
     $unidades = Unidade::all();
-
     return view('autenticacao.cadastro',compact('cursos','unidades')); //redireciona para view de cadastro do aluno
   }
-
   public function storeAluno(Request $request){
-
     $regras = [
       'name' => 'required|string|max:255',
       //'cpf' => ['required','integer','size:11','unique:alunos'],
@@ -152,9 +89,7 @@ class AlunoController extends Controller
       'cursos.required' => 'Por favor, selecione o seu curso',
       'password.required' => 'Por favor, digite uma senha',
       'passowd.min' => 'Por favor, digite uma senha com, no mínimo, 8 dígitos',
-
     ];
-
     //$request->validate([$regras,$mensagens]);
     $request->validate([
       'name' => 'required|string|max:255',
@@ -165,60 +100,41 @@ class AlunoController extends Controller
       'unidade' => ['required'],
       'cursos' => ['required'],
     ]);
-
->>>>>>> bcdcc07322ff96aa8c91318d75be4f29e7695e67
     $usuario = new User();
     $aluno = new Aluno();
     $perfil = new Perfil();
-
-
     //USER
     $usuario->name = $request->input('name');
     $usuario->email = $request->input('email');
     $usuario->password = Hash::make($request->input('password'));
     $usuario->save();
-
-
     //ALUNO
     $aluno->cpf = $request->input('cpf');
     $aluno->user_id = $usuario->id;
     $aluno->save();
-
     //PERFIL
     //$ultimo_cpf = Aluno::where('cpf',$request->cpf)->first();
     //dd($ultimo_cpf->cpf);
     //dd($curso_id->id);
-
     //Default
     $curso = Curso::where('id',$request->cursos)->first();
     $perfil->default = $curso->nome; //Nome do Curso
     //Situacao
     $vinculo = $request->vinculo;
-    if($vinculo==="1"){
+    if($vinculo==="1")
       $perfil->situacao = "Matriculado";
-
     }else {
       $perfil->situacao = "Egresso";
     }
-
     $unidade = Unidade::where('id',$request->unidade)->first();
     //aluno_id
     $perfil->aluno_id = $aluno->id;
     //unidade_id
     $perfil->unidade_id = $unidade->id;
     //curso_id
-
     $perfil->curso_id = $curso->id;
     //dd($perfil);
-
     $perfil->save();
-
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> bcdcc07322ff96aa8c91318d75be4f29e7695e67
     return redirect('/')->with('jsAlert','Usuário Cadastrado com sucesso.');
   }
 }
