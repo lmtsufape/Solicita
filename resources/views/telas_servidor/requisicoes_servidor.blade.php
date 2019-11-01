05d64cbd86691a6da6c4e48712f1599af227050f
pc08 <pc08@lmts.com>
2019-11-01 09:34:22 -0300

    Ordenação da tabela por numero da requisição, nome do aluno e data de requisição. Mudança do nome ID para n° na tabela

--- resources/views/telas_servidor/requisicoes_servidor.blade.php
+++ resources/views/telas_servidor/requisicoes_servidor.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.app')
 
 @section('conteudo')
+
 <!-- @include('componentes.mensagens') -->
     <div class="tabela-centro mx-auto">
-        <table class="table" >
+        <table class="table" id="table" >
             <div class="lmts-primary">
 
               <!-- botão para confirmar seleção -->
@@ -30,13 +31,17 @@
                     </div>
 
                 </th>
-                <th scope="col">Id</th>
-                <th scope="col">CPF</th>
-                <th scope="col">NOME</th>
-                <th scope="col">CURSO</th>
-                <th scope="col">DATA DE REQUISIÇÃO</th>
-                <th scope="col">PRAZO</th>
-                <th scope="col">STATUS</th>
+                <th scope="col" class="titleColumn" onclick="sortTable(0)" style="cursor:pointer">N°
+                  <img src="{{asset('images/sort.png')}}" style="height:15px">
+                </th>
+                <th scope="col" class="titleColumn" >CPF</th>
+                <th scope="col" class="titleColumn" onclick="sortTable(2)" style="cursor:pointer">NOME
+                <img src="{{asset('images/sort.png')}}" style="height:15px"></th>
+                <th scope="col" class="titleColumn" >CURSO</th>
+                <th scope="col" class="titleColumn" onclick="sortTable(4)" style="cursor:pointer">DATA DE REQUISIÇÃO
+                <img src="{{asset('images/sort.png')}}" style="height:15px"></th>
+                <th scope="col" class="titleColumn" >PRAZO</th>
+                <th scope="col" class="titleColumn" >STATUS</th>
 
                 @if($titulo=="Outros" | $titulo=="Programa de Disciplina")
                     <th scope="col">INFORMAÇÕES</th>
@@ -52,7 +57,7 @@
                           <input class="checkboxLinha" type="checkbox" id="checkboxLinha" name="checkboxLinha[]" value="{{$requisicao_documento['id']}}" onclick="">
                         </div>
                     </th>
-                    <td>{{$requisicao_documento['id']}}</td>
+                    <td>{{$loop->iteration}}</td>
                     <td>{{$requisicao_documento['cpf']}}</td>
                     <td>{{$requisicao_documento['nome']}}</td>
                     <td>{{$requisicao_documento['curso']}}</td>
@@ -124,6 +129,62 @@ function getIds(dados){
   }
   return newArray;
 }
+
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("table");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+
 </script>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
