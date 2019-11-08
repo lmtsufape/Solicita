0a6d421ba8d42248134b269d8a410d1a9cec7d5b
JorgeVeloso <jvsveloso@gmail.com>
2019-11-08 08:40:36 -0300

    Modal de alteração de status

--- resources/views/telas_servidor/requisicoes_servidor.blade.php
+++ resources/views/telas_servidor/requisicoes_servidor.blade.php
@@ -66,7 +66,7 @@
                     <td>{{$requisicao_documento['status_data']}}</td>
                     <td>{{$requisicao_documento['status_hora']}}</td>
                     <td>
-                      <a data-toggle="modal" data-target="#modal-mensagem" view_data>
+                      <a data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">
                         <span class="glyphicon glyphicon-remove-circle" style="overflow: hidden; color:red"
                         data-toggle="tooltip; modal" data-placement="top"
                         title="Indeferir pedido." onclick="event.preventDefault()"
@@ -115,23 +115,52 @@
             </tfoot>
           </form>
         </table>
-        <script>
-        $(document).ready(function(){
-          $(document).on('click', '.view_data', function(){
-            alert(user_id);
-            if(user_id!=='')
-              var dados = {
-                user_id:user_id;
-              };
-              $post()
-          }
 
-        )
-        }
-      )
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Falar com @mdo</button>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@fat">Falar com @fat</button>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Falar com @getbootstrap</button>
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Nova mensagem</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <form>
+                  <div class="form-group">
+                    <label for="recipient-name" class="col-form-label">Destinatário:</label>
+                    <input type="text" class="form-control" id="recipient-name">
+                  </div>
+                  <div class="form-group">
+                    <label for="message-text" class="col-form-label">Mensagem:</label>
+                    <textarea class="form-control" id="message-text"></textarea>
+                  </div>
+                </form>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                <button type="button" class="btn btn-primary">Enviar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <script>
+        $('#exampleModal').on('show.bs.modal', function (event) {
+          var button = $(event.relatedTarget) // Botão que acionou o modal
+          var recipient = button.data('whatever') // Extrai informação dos atributos data-*
+          // Se necessário, você pode iniciar uma requisição AJAX aqui e, então, fazer a atualização em um callback.
+          // Atualiza o conteúdo do modal. Nós vamos usar jQuery, aqui. No entanto, você poderia usar uma biblioteca de data binding ou outros métodos.
+          var modal = $(this)
+          modal.find('.modal-title').text('Nova mensagem para ' + recipient)
+          modal.find('.modal-body input').val(recipient)
+        })
         </script>
 <script>
-
 var checkedAll = false;
 var checkBoxs;
 document.getElementById("selectAll").addEventListener("click", function(){
