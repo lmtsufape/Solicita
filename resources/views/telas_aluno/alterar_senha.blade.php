7db700dd220e26708539b8a22d6a7806d52e019d
ihagoSantos <ihagog12@gmail.com>
2020-03-10 15:49:30 -0300

    layout editar info e senha aluno

--- resources/views/telas_aluno/alterar_senha.blade.php
+++ resources/views/telas_aluno/alterar_senha.blade.php
@@ -1,97 +1,93 @@
 @extends('layouts.app')
 
 @section('conteudo')
-<div class="background">
-    <div class="background" style="height: 840px">
-        <div class="centro" style="height: 840px">
-          <div>@include('componentes.mensagens')</div>
-                <h2 class="row d-flex justify-content-center" >Alterar Senha</h2>
+<div class="container-fluid background-blue" style="">
+    <div class="row justify-content-center">
+        <div class="col-sm-3">
 
-                <form action="{{  route('alterar-senha')  }}" method="POST">
+          <div class="card card-cadastro">
+            <div class="card-body">
+              <div class="row justify-content-center">
+                  <h2>Alterar Senha</h2>
+              </div>
+              <form action="{{  route('alterar-senha')  }}" method="POST">
 
-                  @csrf
-                  <div class="form-group">
+                @csrf
 
+                <div class="row justify-content-center">
+                  <div class="col-sm-12">
+                    <label for="password" class="field a-field a-field_a3 page__field" >
+                    <input id="password-old" type="password" class="form-control @error('password-old') is-invalid @enderror field__input a-field__input"
+                    name="atual" required autocomplete="current-password" placeholder="Senha Antiga" >
 
-                    <!-- Form Senha Antiga -->
-                    <div class="form-group row formulario-centro">
+                    <span class="a-field__label-wrap">
+                        <span class="a-field__label">Senha atual</span>
+                    </span>
+                    </label>
+                    @error('password-old')
+                    <span class="invalid-feedback" role="alert" style="overflow: visible; display:block;">
+                    <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                  </div>
+                </div>
 
-                        <div class="col-md-9">
-                            <label for="password" class="field a-field a-field_a3 page__field" >
-                            <input id="password-old" type="password" class="form-control @error('password-old') is-invalid @enderror field__input a-field__input"
-                            name="atual" required autocomplete="current-password" placeholder="Senha Antiga" >
+                <div class="row justify-content-center">
+                  <div class="col-sm-12">
+                    <label for="password" class="field a-field a-field_a3 page__field" >
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror field__input a-field__input"
+                    name="password" required autocomplete="current-password" placeholder="Nova Senha" >
 
-                            <span class="a-field__label-wrap">
-                                <span class="a-field__label">Senha atual</span>
-                            </span>
-                            </label>
-                            @error('password-old')
-                            <span class="invalid-feedback" role="alert" style="overflow: visible; display:block;">
-                            <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
+                    <span class="a-field__label-wrap">
+                        <span class="a-field__label">Nova Senha</span>
+                    </span>
+                    </label>
+                    @error('password')
+                    <span class="invalid-feedback" role="alert" style="overflow: visible; display:block;">
+                    <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                  </div>
+                </div>
+
+                <div class="row justify-content-center">
+                  <div class="col-sm-12">
+                    <label for="password-confirm" class="field a-field a-field_a3 page__field" >
+                    <input id="password-confirm" type="password" class="form-control @error('password-confirm') is-invalid @enderror field__input a-field__input"
+                    name="password_confirmation" required autocomplete="new-password" placeholder="Confirmar Nova Senha">
+
+                    <span class="a-field__label-wrap">
+                        <span class="a-field__label">Confirmar Nova Senha</span>
+                    </span>
+                    </label>
+                    @error('password-confirm')
+                    <span class="invalid-feedback" role="alert" style="overflow: visible; display:block;">
+                    <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                  </div>
+                </div>
+                <!-- Botões -->
+                <div class="form-group row justify-content-center" style="margin-top:60px">
+                    <div class="col-sm-6">
+                    <a class="btn btn-light btn-cadastro-primary" href="{{  route('perfil-aluno')}}" >Voltar</a>
                     </div>
 
-
-                    <!-- Form Senha -->
-                    <div class="form-group row formulario-centro">
-
-                        <div class="col-md-9">
-                            <label for="password" class="field a-field a-field_a3 page__field" >
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror field__input a-field__input"
-                            name="password" required autocomplete="current-password" placeholder="Nova Senha" >
-
-                            <span class="a-field__label-wrap">
-                                <span class="a-field__label">Nova Senha</span>
-                            </span>
-                            </label>
-                            @error('password')
-                            <span class="invalid-feedback" role="alert" style="overflow: visible; display:block;">
-                            <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
+                    <div class="col-sm-6">
+                        <button type="submit" class="btn lmts-primary btn-cadastro-primary">
+                            {{ __('Salvar') }}
+                        </button>
                     </div>
 
-                    <!-- Form Confirmar Senha -->
-                    <div class="form-group row formulario-centro">
+                </div>
 
-                        <div class="col-md-9">
-                            <label for="password-confirm" class="field a-field a-field_a3 page__field" >
-                            <input id="password-confirm" type="password" class="form-control @error('password-confirm') is-invalid @enderror field__input a-field__input"
-                            name="password_confirmation" required autocomplete="new-password" placeholder="Confirmar Nova Senha">
-
-                            <span class="a-field__label-wrap">
-                                <span class="a-field__label">Confirmar Nova Senha</span>
-                            </span>
-                            </label>
-                            @error('password-confirm')
-                            <span class="invalid-feedback" role="alert" style="overflow: visible; display:block;">
-                            <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                    </div>
-
-
-                    <!-- Botões -->
-                    <div class="form-group row mb-0 justify-content-center ">
-                        <div class="row " style="margin-top:20px; margin-left:-30px">
-                                <div class="col-md-6 " style="">
-                                    <a class="menu-principal" href="{{  route('perfil-aluno')}}" style="color: #1B2E4F; margin-left: -20px">Voltar</a>
-                                </div>
-
-                                <div class="col-md-6 " style="margin-left: -30px; margin-top: -4px">
-                                    <button type="submit" class="btn btn-primary"  style="margin-left: 60px;background-color: #1B2E4F; border-color: #d3e0e9">
-                                        {{ __('Atualizar') }}
-                                    </button>
-                                </div>
-                        </div>
-
-                    </div>
 
               </form>
+            </div>
           </div>
-      </div>
+        </div>
+    </div>
+</div>
+
+
 @endsection
--- resources/views/telas_aluno/editar_info_aluno.blade.php
+++ resources/views/telas_aluno/editar_info_aluno.blade.php
@@ -1,71 +1,78 @@
 @extends('layouts.app')
 
 @section('conteudo')
-<div class="background">
 
+<div class="container-fluid background-blue" style="">
+    <div class="row justify-content-center">
+        <div class="col-sm-3">
 
-    <div class="background" style="height: 840px">
-        <div class="centro" style="height: 840px">
-          <div>@include('componentes.mensagens')</div>
-                <h2 class="row d-flex justify-content-center" >Editar Perfil</h2>
+          <div class="card card-cadastro">
+            <div class="card-body">
+              <div class="row justify-content-center">
+                  <h2>Editar Informações</h2>
+              </div>
 
-                <form action="{{  route('editar-info')  }}" method="POST">
+              <form action="{{  route('editar-info')  }}" method="POST">
+                @csrf
 
-                  @csrf
-                  <div class="form-group">
-                    <!-- Form Nome -->
-                    <div class="form-group row formulario-centro">
-                        <div class="col-md-9">
-                            <label for="name" class="field a-field a-field_a3 page__field ">
-                            <input id="name" type="name" class="form-control @error('name') is-invalid @enderror field__input a-field__input"
-                            name="name" value="{{ $user->name }}" required autocomplete="name" autofocus placeholder="Nome Completo">
+                <div class="row justify-content-center">
+                  <div class="col-sm-12">
+                    <label for="name" class="field a-field a-field_a3 page__field ">
+                    <input id="name" type="name" class="form-control @error('name') is-invalid @enderror field__input a-field__input"
+                    name="name" value="{{ $user->name }}" required autocomplete="name" autofocus placeholder="Nome Completo">
 
-                            <span class="a-field__label-wrap">
-                                <span class="a-field__label">Nome Completo</span>
-                            </span>
-                            </label>
-                            @error('name')
-                            <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
-                            <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
+                    <span class="a-field__label-wrap">
+                        <span class="a-field__label">Nome Completo</span>
+                    </span>
+                    </label>
+                    @error('name')
+                    <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
+                    <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                  </div>
+                </div>
+
+                <div class="row justify-content-center">
+                  <div class="col-sm-12">
+                    <label for="email" class="field a-field a-field_a3 page__field ">
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror field__input a-field__input"
+                    name="email" value="{{ $user->email }}" required autocomplete="email" autofocus placeholder="E-Mail">
+
+                    <span class="a-field__label-wrap">
+                        <span class="a-field__label">E-mail</span>
+                    </span>
+                    </label>
+                    @error('email')
+                    <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
+                    <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                  </div>
+                </div>
+
+                <!-- Botões -->
+                <div class="form-group row justify-content-center" style="margin-top:60px">
+                    <div class="col-sm-6">
+                    <a class="btn btn-light btn-cadastro-primary" href="{{  route('perfil-aluno')}}" >Voltar</a>
                     </div>
-                    <!-- Form E-mail -->
-                    <div class="form-group row formulario-centro">
 
-                        <div class="col-md-9">
-                            <label for="email" class="field a-field a-field_a3 page__field ">
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror field__input a-field__input"
-                            name="email" value="{{ $user->email }}" required autocomplete="email" autofocus placeholder="E-Mail">
-
-                            <span class="a-field__label-wrap">
-                                <span class="a-field__label">E-mail</span>
-                            </span>
-                            </label>
-                            @error('email')
-                            <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
-                            <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
+                    <div class="col-sm-6">
+                        <button type="submit" class="btn lmts-primary btn-cadastro-primary">
+                            {{ __('Salvar') }}
+                        </button>
                     </div>
 
-                    <!-- Botões -->
-                    <div class="form-group row mb-0 justify-content-center ">
-                        <div class="row " style="margin-top:20px; margin-left:-30px">
-                                <div class="col-md-6 " style="">
-                                    <a class="menu-principal" href="{{  route('perfil-aluno')}}" style="color: #1B2E4F; margin-left: -20px">Voltar</a>
-                                </div>
+                </div>
 
-                                <div class="col-md-6 " style="margin-left: -30px; margin-top: -4px">
-                                    <button type="submit" class="btn btn-primary"  style="margin-left: 60px;background-color: #1B2E4F; border-color: #d3e0e9">
-                                        {{ __('Atualizar') }}
-                                    </button>
-                                </div>
-                        </div>
-                    </div>
+
               </form>
+            </div>
           </div>
-      </div>
+        </div>
+    </div>
+</div>
+
+
+
 @endsection
