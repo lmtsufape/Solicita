13a17ca14ac73e9601f6b27177cc0c1846fa88ec
ihagoSantos <ihagog12@gmail.com>
2020-03-10 15:33:53 -0300

    editar perfil

--- resources/views/telas_aluno/adiciona_perfil_aluno.blade.php
+++ resources/views/telas_aluno/adiciona_perfil_aluno.blade.php
@@ -1,78 +1,97 @@
 @extends('layouts.app')
 
 @section('conteudo')
-<div class="background">
-    <div class="background" style="height: 840px">
-        <div class="centro" style="height: 840px">
-          <div>@include('componentes.mensagens')</div>
-                <h2 class="row d-flex justify-content-center">Adicionar Perfil</h2>
-                <form action="{{  route('salva-novo-perfil-aluno')  }}" method="POST">
-                  @csrf
-                  <div class="form-group">
-                    <div class="form-group row formulario-centro">
-                      <div class="col-md-9">
-                        <label for="vinculo" style="margin-left:25%">Tipo de vínculo</label>
-                        <select name="vinculo" id="vinculo" class="browser-default custom-select custom-select-lg mb-3" style="width: 16rem; margin-left:25%">
-                          <option value="1" selected>Matriculado</option>
-                          <option value="2">Egresso</option>
-                          <option value="3">Especial</option>
-                          <option value="4">REMT - Regime Especial de Movimentação Temporária</option>
-                          <option value="5">Desistente</option>
-                          <option value="6">Matrícula Trancada</option>
-                          <option value="7">Intercâmbio</option>
-                            @error('vinculo')
-                            <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
-                            <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </select>
-                      <!-- Unidade Acadêmica-->
-                      <label for="unidade" style="margin-left:25%">Unidade Acadêmica</label>
-                      <select name="unidade" id="unidade"class="browser-default custom-select custom-select-lg mb-1" style="width: 16rem; margin-left:25%">
-                        @foreach($unidades as $unidade)
-                        <option value="{{$unidade->id}}">{{$unidade->nome}}</option>
-                        @endforeach
-                        @error('unidade')
-                        <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
-                        <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                      </select>
+<div class="container-fluid background-blue">
+  <div class="row justify-content-center">
+    <div>@include('componentes.mensagens')</div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col-sm-3">
+      <div class="card card-cadastro">
+        <div class="card-body">
+          <div class="row justify-content-center">
+            <h2>Cadastrar Perfil</h2>
+            <form action="{{  route('salva-novo-perfil-aluno')  }}" method="POST">
+              @csrf
+              <div class="row justify-content-center">
+                <div class="col-sm-12">
+                  <label for="vinculo">Tipo de vínculo</label>
+                  <select name="vinculo" id="vinculo" class="browser-default custom-select custom-select-lg mb-3">
+                    <option value="1" selected>Matriculado</option>
+                    <option value="2">Egresso</option>
+                    <option value="3">Especial</option>
+                    <option value="4">REMT - Regime Especial de Movimentação Temporária</option>
+                    <option value="5">Desistente</option>
+                    <option value="6">Matrícula Trancada</option>
+                    <option value="7">Intercâmbio</option>
+                      @error('vinculo')
+                      <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
+                      <strong>{{ $message }}</strong>
+                      </span>
+                      @enderror
+                  </select>
+                </div>
+              </div>
 
-                      <!-- Cursos-->
-                      <label for="cursos" style="margin-left:25%">Curso</label>
-                      <select name="curso" id="cursos" class="browser-default custom-select custom-select-lg mb-1" style="width: 16rem; margin-left:25%">
-                        <options selected>Curso</option>
-                          @foreach($cursos as $curso)
-                          <option value="{{$curso->id}}">{{$curso->nome}}</option>
-                          @endforeach
-                          @error('curso')
-                          <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
-                          <strong>{{ $message }}</strong>
-                          </span>
-                          @enderror
-                      </select></br>
-                      <input type="checkbox" name="selecaoPadrao"  value="true">Definir como padrão
-                      </input>
-                      </div>
-                    </div>
-                    <!-- Botões -->
-                    <div class="form-group row mb-0 justify-content-center ">
-                        <div class="row " style="margin-top:5%; margin-left:-30px">
-                                <div class="col-md-6 " style="">
-                                    <a class="menu-principal" href="{{  route('perfil-aluno')}}" style="color: #1B2E4F; margin-left: -20px">Voltar</a>
-                                </div>
+              <div class="row justify-content-center">
+                <div class="col-sm-12">
+                  <!-- Unidade Acadêmica-->
+                  <label for="unidade">Unidade Acadêmica</label>
+                  <select name="unidade" id="unidade"class="browser-default custom-select custom-select-lg mb-1">
+                    @foreach($unidades as $unidade)
+                    <option value="{{$unidade->id}}">{{$unidade->nome}}</option>
+                    @endforeach
+                    @error('unidade')
+                    <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
+                    <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                  </select>
 
-                                <div class="col-md-6 " style="margin-left: -30px; margin-top: -4%">
-                                    <button type="submit" class="btn btn-primary"  href="{{  route("salva-novo-perfil-aluno")}}"
-                                      style="margin-left: 60px;background-color: #1B2E4F; border-color: #d3e0e9">
-                                        {{ __('Salvar') }}
-                                    </button>
-                                </div>
-                        </div>
+                </div>
+              </div>
 
-                    </div>
-              </form>
+              <div class="row justify-content-center">
+                <div class="col-sm-12">
+                  <!-- Cursos-->
+                  <label for="cursos">Curso</label>
+                  <select name="curso" id="cursos" class="browser-default custom-select custom-select-lg mb-1">
+                    <options selected>Curso</option>
+                      @foreach($cursos as $curso)
+                      <option value="{{$curso->id}}">{{$curso->nome}}</option>
+                      @endforeach
+                      @error('curso')
+                      <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
+                      <strong>{{ $message }}</strong>
+                      </span>
+                      @enderror
+                  </select>
+
+                  <input style="margin-top:20px" type="checkbox" name="selecaoPadrao"  value="true">Definir como padrão
+                  </input>
+                </div>
+              </div>
+
+              <!-- Botões -->
+              <div class="form-group row justify-content-center" style="margin-top:60px">
+                  <div class="col-sm-6">
+                  <a class="btn btn-light btn-cadastro-primary" href="{{  route('perfil-aluno')}}" >Voltar</a>
+                  </div>
+
+                  <div class="col-sm-6">
+                      <button type="submit" class="btn lmts-primary btn-cadastro-primary">
+                          {{ __('Cadastrar') }}
+                      </button>
+                  </div>
+
+              </div>
+            </form>
+
           </div>
+        </div>
       </div>
+
+    </div>
+  </div>
+
 @endsection
